447914e1fa6a0e356f18632674a53f25dfa96ed0
alena <bonda5229@gmail.com>
2026-09-05 15:42:26 +0200

    setup docker and add CI workflow

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { PrismaExceptionFilter } from './common/prisma-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalFilters(new PrismaExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
 }
