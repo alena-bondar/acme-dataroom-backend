5803485d52404d31bec85c7f137746b1783c7085
alena <bonda5229@gmail.com>
2026-09-05 16:31:48 +0200

    fix tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -5,10 +5,23 @@ import { UsersService } from './users.service';
 describe('UsersController', () => {
   let controller: UsersController;
 
+  const usersServiceMock = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    remove: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers: [UsersService],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: usersServiceMock,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,12 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { PrismaService } from '../prisma.service';
 
 describe('UsersService', () => {
   let service: UsersService;
 
+  const prismaMock = {
+    user: {
+      findMany: jest.fn(),
+      findUnique: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+      delete: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: PrismaService,
+          useValue: prismaMock,
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
